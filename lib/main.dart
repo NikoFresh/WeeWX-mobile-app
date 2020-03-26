c5c0436d712166724e3a8cb5919bbc5293d6c505
NikoFresh <nicolo.frescura@gmail.com>
2020-03-26 18:25:20 +0100

    Fix internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -6,57 +6,78 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
 
-class MyAppState extends State<MyApp> {
-  int _selectedTab = 0;
-  final _pageOptions = [
-    CurrentHome(),
-    Text('I\'m useless'),
-    Text('I\'m useless too'),
-    //Text('I\'m useless'),
-  ];
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'WeeWX weather app';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       localizationsDelegates: [S.delegate],
       supportedLocales: S.delegate.supportedLocales,
-      title: 'Weewx weather app',
+      title: _title,
       theme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: const Color(0xFF131825),
           accentColor: const Color(0xFF2D3446),
           textTheme: TextTheme()),
-      home: Scaffold(
-        body: SafeArea(child: _pageOptions[_selectedTab]),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedTab,
-          onTap: (int index) {
-            setState(() {
-              _selectedTab = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.alarm),
-              title: Text('Current'),
-              backgroundColor: Theme.of(context).primaryColor,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              title: Text('Summary'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
-          ],
-        ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+  @override
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  List<Widget> _widgetOptions = <Widget>[
+    CurrentHome(),
+    Text(
+      'I\'m useless',
+      style: optionStyle,
+    ),
+    Text(
+      'I\'m useless too',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            title: Text('Summary'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
